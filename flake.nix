49cee38d05efa39b5f879fd7af501df53e5ced08
Luna Skye <sepshuncontact@gmail.com>
2024-11-11 02:01:02 -0500

    fix: remove hm references

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
   };
 
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: let
+  outputs = inputs@{ self, nixpkgs, ... }: let
     inherit (inputs.nixpkgs) lib;
-    utils = import ./lib { inherit self; inherit inputs; };
+    bead = import ./lib { inherit self; inherit inputs; };
 
     # Returns a NixOS System configured for the specified host with overlaid home-manager users
     # 
@@ -17,7 +17,7 @@
     # - `host`: The name of the host system
     # - `users`: A list of string names for home-manager users to overlay onto the system
     mkNixosHost = host: lib.nixosSystem {
-      specialArgs = { inherit inputs; inherit utils; };
+      specialArgs = { inherit inputs; inherit bead; };
       system = "x86_64-linux";
       modules = [
         (./hosts + "/${host}/hardware.nix")
