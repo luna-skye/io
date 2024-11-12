7dbec083bde7879bbadd3d0851f56b1686dd8792
Luna Skye <sepshuncontact@gmail.com>
2024-11-12 18:19:18 -0500

    refactor(hosts/bebop): remove redundant opt

--- hosts/bebop/configuration.nix
+++ hosts/bebop/configuration.nix
@@ -23,8 +23,6 @@
     ];
   };
 
-  nixpkgs.config.allowUnfree = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
